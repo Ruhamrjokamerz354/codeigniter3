6e5494deef72709f01cdcbabca80f442058f8009
Ruhamrjokamerz354 <ruhamdjokamerz2gmail.com>
2022-09-22 15:15:01 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+    echo "<h1> Perkenalkan </h1>";
+    echo "Nama Saya Ruham,
+        saya tinggal di daerah Kepuh, Klitren
+        Asal dari Magelang";    
+    }
+}
